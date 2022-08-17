ecf2cc393a1622b873c0ac0bc1464dbdc17ccd89
petelab <petelab@users.noreply.github.com>
2022-08-17 10:14:15 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <CharacterEditor />
+      <div className="perspective"></div>
       <Footer />
     </>
   );
